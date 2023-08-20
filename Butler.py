7e3f2964de9bf1dc2ac508e04ae579c300bf0e5d
WhiteStorm <andrzejmmm1@gmail.com>
2023-08-20 18:37:07 +0200

    New program UI
    Refactor the code to implement a new program UI. The changes include:
    - Added keyboard module for user input
    - Updated version() function to use increment_version() and handle version updates
    - Modified platform() function to display platform options
    - Updated windows(), linux(), mac(), and butler() functions to accept additional argument 'apps'
    - Implemented clear_screen(), display_menu(), and main() functions for the new UI flow

--- Butler.py
+++ Butler.py
@@ -1,131 +1,264 @@
-import os, sys
+import os, sys, keyboard
 import requests
 from Butler_github import github
 from Butler_jira import jira
-# from Butler_build import Create, Remove
+from Butler_build import Create, Remove
 from Butler_constants import TColors, Config
 
 # **************
 # * Version
 # **************
 
-def version():
-    f = open(os.path.join(sys.path[0], "Butler_version.txt"),"r")
-    currVersion = f.read()
-    print(TColors.HEADER+"Current version is: ",currVersion,"\n"+TColors.ENDC)
-    print("[1] - update minor")
-    print("[2] - update major")
-    print("[3] - update patch")
-    print("[0] - don't change\n")
-    action = input("Type Option: ")
-    print("\n")
-    if action == '1':
-        tmp = currVersion.split('.')
-        tmp[0]=str(int(tmp[0])+1)
-        currVersion = tmp[0]+".0.0"
-    elif action == '2':
-        tmp = currVersion.split('.')
-        tmp[1]=str(int(tmp[1])+1)
-        currVersion = tmp[0]+"."+tmp[1]+".0"
-    elif action == '3':
-        tmp = currVersion.split('.')
-        tmp[2]=str(int(tmp[2])+1)
-        currVersion = tmp[0]+"."+tmp[1]+"."+tmp[2]
-    elif action != '0':
-        return -1
-    f.close()
-    f = open(os.path.join(sys.path[0], "Butler_version.txt"),"w")
-    f.write(currVersion)
-    f.close()
-    return currVersion
+def increment_version(current_version, index_to_change):
+    # Rozbij obecną wersję na części
+    parts = current_version.split('.')
+    
+    # Sprawdź, czy podany indeks jest prawidłowy
+    if 0 <= index_to_change < len(parts):
+        # Zwiększ wartość na danym indeksie
+        parts[index_to_change] = str(int(parts[index_to_change]) + 1)
+        
+        # Wyzeruj kolejne wartości po danym indeksie
+        for i in range(index_to_change + 1, len(parts)):
+            parts[i] = '0'
+        
+        # Złącz części z powrotem w nową wersję
+        new_version = '.'.join(parts)
+        return new_version
+    else:
+        return current_version  # Indeks nieprawidłowy
     
 
 # **************
 # * Platforms
 # **************
 
-def platform():
-    print(TColors.HEADER+"Select platform for push build to\n"+TColors.ENDC)
-    print("[1] - Windows")
-    print("[2] - Linux")
-    print("[3] - Mac")
-    print("[4] - All at once")
-    print("[0] - Exit")
-    action = input("Type Option: ")
-    return action
-        
-
-
-def windows(currVersion, body):
+def windows(currVersion, body, apps):
     #? Itch
-    os.system("butler.exe push windows "+Config["ITCH_SITE_NAME"]+":win --if-changed --userversion "+currVersion)
+    if "itch.io" in apps:
+        os.system("butler.exe push windows "+Config["ITCH_SITE_NAME"]+":win --if-changed --userversion "+currVersion)
     #? Discord
-    payload = {"content":"Version: "+currVersion+" was pushed to itch on Windows channel"}        
-    requests.post(Config["DISCORD_HOOK"], data=payload)
+    if "discord" in apps:
+        payload = {"content":"Version: "+currVersion+" was pushed to itch on Windows channel"}        
+        requests.post(Config["DISCORD_HOOK"], data=payload)
     #? Github
-    github(Config["PROJECT_NAME"],currVersion,{"Windows"},body ,True,False,None)
+    if "github" in apps:
+        github(Config["PROJECT_NAME"],currVersion,{"Windows"},body ,True,False,None)
     #? Jira
-    jira(currVersion, body)
+    if "jira" in apps:
+        jira(currVersion, body)
 
-def linux(currVersion, body):  
+def linux(currVersion, body, apps):  
     #? Itch
-    os.system("butler.exe push linux "+Config["ITCH_SITE_NAME"]+":linux --if-changed --userversion "+currVersion)
+    if "itch.io" in apps:
+        os.system("butler.exe push linux "+Config["ITCH_SITE_NAME"]+":linux --if-changed --userversion "+currVersion)
     #? Discord
-    payload = {"content":"Version: "+currVersion+" was pushed to itch on Linux channel"}        
-    requests.post(Config["DISCORD_HOOK"], data=payload)
+    if "discord" in apps:
+        payload = {"content":"Version: "+currVersion+" was pushed to itch on Linux channel"}        
+        requests.post(Config["DISCORD_HOOK"], data=payload)
     #? Github
-    github(Config["PROJECT_NAME"],currVersion,{"Linux"},body ,True,False,None)
+    if "github" in apps:
+        github(Config["PROJECT_NAME"],currVersion,{"Linux"},body ,True,False,None)
     #? Jira
-    jira(currVersion, body)
+    if "jira" in apps:
+        jira(currVersion, body)
 
-def mac(currVersion, body):
+def mac(currVersion, body, apps):
     #? Itch
-    os.system("butler push mac "+Config["ITCH_SITE_NAME"]+":mac --if-changed --userversion "+currVersion)
+    if "itch.io" in apps:
+        os.system("butler push mac "+Config["ITCH_SITE_NAME"]+":mac --if-changed --userversion "+currVersion)
     #? Discord
-    payload = {"content":"Version: "+currVersion+" was pushed to itch on Mac channel"}        
-    requests.post(Config["DISCORD_HOOK"], data=payload)
+    if "discord" in apps:
+        payload = {"content":"Version: "+currVersion+" was pushed to itch on Mac channel"}        
+        requests.post(Config["DISCORD_HOOK"], data=payload)
     #? Github
-    github(Config["PROJECT_NAME"],currVersion,{"Mac"},body ,True,False,None)
+    if "github" in apps:
+        github(Config["PROJECT_NAME"],currVersion,{"Mac"},body ,True,False,None)
     #? Jira
-    jira(currVersion, body)
+    if "jira" in apps:
+        jira(currVersion, body)
 
-# def butler(currVersion, body):
-#     #? Github
-#     Create()
-#     github(ProjectName,currVersion,{"Build"},body ,False,False,None,TokenPath)
-#     Remove()
+def butler(currVersion, body):
+    #? Github
+    Create()
+    github(Config["PROJECT_NAME"],currVersion,{"Build"},body ,False,False,None)
+    Remove()
 
 # **************
 # * Main loop
 # **************
 
-if __name__ == "__main__":
-    action = '-1'
-    while(action!='0'):
-        action = platform()
-        if action == '0':
-            continue
-        body = input("Write text for release body:\n")
-        # body = ""
-        print("\n")
-        if action == '1':
-            windows(version(), body)
-        elif action == '2':
-            linux(version(), body) 
-        elif action == '3':
-            mac(version(), body)
-        elif action == '4':
-            windows(version(), body)
-            linux(version(), body) 
-            mac(version(), body)
-        # elif action == '5':
-        #     wait = input(TColors.WARNING+"Comment butler code then press Enter to continue."+TColors.ENDC)
-        #     butler(version(), body)
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def display_menu(menu_options, selected_index, prefix = "", suffix = "", selected_indexes = []):
+    print(TColors.BOLD+"\n")
+    print("  ______              _              ")
+    print(" (____  \\         _  | |             ")
+    print("  ____)  )_   _ _| |_| | _____  ____ ")
+    print(" |  __  (| | | (_   _) || ___ |/ ___)")
+    print(" | |__)  ) |_| | | |_| || ____| |    ")
+    print(" |______/|____/   \\__)\\_)_____)_|    ")
+    print("                        by WhiteStorm")
+    print(f"{TColors.ENDC}\n\n{TColors.HEADER}{prefix}{TColors.ENDC}\n")
+    for index, option in enumerate(menu_options):
+        if option in selected_indexes:
+            print(TColors.OKBLUE, end="")
+        if index == selected_index:
+            print(f"    [{index + 1}] -> {option}")
         else:
-            print(TColors.WARNING+"Wrong option"+TColors.ENDC)
-        # print(version())
-        wait = input(TColors.OKBLUE+"Press Enter to continue."+TColors.ENDC)
-        os.system("cls")
+            print(f"     {index + 1}    {option}")
+        print(TColors.ENDC, end="")
+    print(suffix)
+
+def main():
+
+    if len(sys.argv)>1:
+        menu_options = [
+            "Butler",
+            "Windows",
+            "Linux",
+            "Mac",
+            "All at once",
+            "Exit"
+        ]
+    else:
+        menu_options = [
+            "Windows",
+            "Linux",
+            "Mac",
+            "All at once",
+            "Exit"
+        ]
+    
+    version_options = [
+        "update minor",
+        "update major",
+        "update patch",
+        "don't change",
+        "go back"
+    ]
+
+    release_options = [
+        "itch.io",
+        "discord",
+        "github",
+        "jira",
+        "submit",
+        "go back"
+    ]
+    
+    selected_index = 0
+    selected_view = 0
+    selected_platform = ""
+    selected_version = ""
+    selected_releases = []
+    error = ""
+    version = ""
+    
+    while True:
+        clear_screen()
+        if selected_view == 0:
+            display_menu(menu_options, selected_index, prefix="Select platform to release on:", suffix=error)
+            menu_len = len(menu_options)
+        elif selected_view == 1:
+            f = open(os.path.join(sys.path[0], "Butler_version.txt"),"r")
+            version = f.read()
+            f.close()
+            display_menu(version_options, selected_index, prefix="Current Version is: v"+version, suffix=error+f"\nSelected platform: {selected_platform}")
+            menu_len = len(version_options)
+        elif selected_view == 2:
+            display_menu(release_options, selected_index, prefix="Select app you wanna release to:", suffix=error+f"\nSelected platform: {selected_platform}, Selected version operation: {selected_version}", selected_indexes=selected_releases)
+            menu_len = len(release_options)
+        elif selected_view == 3:
+            display_menu([], 0, suffix="Type description you wanna parse to release:", prefix=error+f"\nSelected platform: {selected_platform}, Selected version operation: {selected_version}")
+        
+        if selected_view<3:
+            key_event = keyboard.read_event(suppress=True)
+            if key_event.event_type == keyboard.KEY_DOWN:
+                error=""
+                if key_event.name == "w":
+                    selected_index = (selected_index - 1) % menu_len
+                elif key_event.name == "s":
+                    selected_index = (selected_index + 1) % menu_len
+                elif key_event.name == "up":
+                    selected_index = (selected_index - 1) % menu_len
+                elif key_event.name == "down":
+                    selected_index = (selected_index + 1) % menu_len
+                elif key_event.name == "enter":
+                    if selected_view == 0:
+                        selected_option = menu_options[selected_index]
+                        selected_index = 0
+                        print(f"Selected: {selected_option}")
+                        if selected_option == "Exit":
+                            break
+                        else:
+                            selected_view = 1
+                            selected_platform = selected_option
+                    elif selected_view == 1:
+                        selected_option = version_options[selected_index]
+                        selected_index = 0
+                        if selected_option == "go back":
+                            selected_view = 0
+                            selected_version = ""
+                        else:
+                            selected_view = 2
+                            selected_version = selected_option
+                    elif selected_view == 2:
+                        selected_option = release_options[selected_index]
+                        if selected_option == "go back":
+                            selected_index = 0
+                            selected_view = 1
+                            selected_releases = {}
+                        elif selected_option == "submit":
+                            if len(selected_releases):
+                                selected_view = 3
+                            else:
+                                error =  TColors.WARNING+"\nApp not selected\n"+TColors.ENDC
+                        else:
+                            if selected_option in selected_releases:
+                                selected_releases.remove(selected_option)
+                            else:
+                                selected_releases.append(selected_option)
+        else:
+            body = input()
+            version = increment_version(version, version_options.index(selected_version))
+
+            try:
+                if selected_platform == "Butler":
+                    butler(version,body,selected_releases)
+                elif selected_platform == "Windows":   
+                    windows(version,body,selected_releases)   
+                elif selected_platform == "Linux":
+                    linux(version,body,selected_releases)
+                elif selected_platform == "Mac":
+                    mac(version,body,selected_releases)
+                elif selected_platform == "All at once":
+                    windows(version,body,selected_releases)
+                    linux(version,body,selected_releases)
+                    mac(version,body,selected_releases)
+
+                # save new version
+                f = open(os.path.join(sys.path[0], "Butler_version.txt"),"w")
+                f.write(version)
+                f.close()
+
+            except:
+                print()
+            
+
+            wait = input("Press enter to continue...")
+            selected_index = 0
+            selected_view = 0
+            selected_platform = ""
+            selected_version = ""
+            selected_releases = []
+            error = ""
+                
+
+if __name__ == "__main__":
+    main()
+
 
 
 
